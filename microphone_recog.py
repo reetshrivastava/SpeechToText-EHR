2af2074f004e104efb9bbbcf754c57e21fc5da7c
Reet Shrivastava <reet.shrivastava@hashedin.com>
2018-02-09 16:21:18 +0530

    modifications

--- microphone_recog.py
+++ microphone_recog.py
@@ -11,16 +11,12 @@ def translate(audio):
         # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
         # instead of `r.recognize_google(audio)`
         print(r.recognize_google(audio))
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 # obtain audio from the microphone
 r = sr.Recognizer()
+print("Say something!")
 with sr.Microphone() as source:
-    while(1):
-    	print("Say something!")
+    while True:
     	audio = r.listen(source)
     	translate(audio)
 
